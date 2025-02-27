325e6409d9ed11718cc9bf12bc0b1d0987ea246d
Anton Zhukov <a.zhukov@intec-balance.ru>
2025-02-27 17:08:51 +0300

    added constants and input validation

--- char_creator.py
+++ char_creator.py
@@ -1,3 +1,4 @@
+from mmo_constants import *
 from rases import Elf, Dwarf
 from profs import Archer, Warrior
 
@@ -21,7 +22,35 @@ class CharCreate(Elf, Dwarf, Archer, Warrior):
         self.weapon_names = self.weapons[0]["name"], self.weapons[1]["name"],
         self.update_info()
         self.max_hp = self.hp
+        self.info["Класс"] = self.prof
+gender = None
+rase = None
+prof = None
 
-character = CharCreate(input('Введите пол - "Мужчина"/"Женщина": '), input('Введите расу - "Эльф"/"Дварф": '), input('Введите класс - "Лучник"/"Воин": '))
+while True:
+    x = input('Введите пол - "Мужчина"/"Женщина": ')
+    if x in VALID_GENDERS:
+        gender = x
+        break
+    else:
+        print('Введите существующий пол - "Мужчина"/"Женщина": ')
+
+while True:
+    x = input('Введите расу - "Эльф"/"Дварф": ')
+    if x in VALID_RASES:
+        rase = x
+        break
+    else:
+        print('Введите существующую расу - "Эльф"/"Дварф": ')
+
+while True:
+    x = input('Введите класс - "Лучник"/"Воин": ')
+    if x in VALID_PROFS:
+        prof = x
+        break
+    else:
+        print('Введите существующий класс - "Воин"/"Лучник": ')
+
+character = CharCreate(gender, rase, prof)
 
 print(character.info)
